5f4f8e456d6e66f15b74e85074f20224068d9a8c
sorvigolovaa <sbknugaara@gmail.com>
2023-04-30 20:54:04 +0500

    feat: add font, style, js

--- app.js
+++ app.js
@@ -1,168 +1,231 @@
-// Создаем элементы <div> для виртуальной клавиатуры и экрана
-const keyboard = document.createElement('div');
-keyboard.classList.add('keyboard');
+const keys = [
+  { label: 'ё', en: '`', keyCode: '192' },
+  { label: '1', en: '1', keyCode: '49' },
+  { label: '2', en: '2', keyCode: '50' },
+  { label: '3', en: '3', keyCode: '51' },
+  { label: '4', en: '4', keyCode: '52' },
+  { label: '5', en: '5', keyCode: '53' },
+  { label: '6', en: '6', keyCode: '54' },
+  { label: '7', en: '7', keyCode: '55' },
+  { label: '8', en: '8', keyCode: '56' },
+  { label: '9', en: '9', keyCode: '57' },
+  { label: '0', en: '0', keyCode: '48' },
+  { label: '-', en: '-', keyCode: '189' },
+  { label: '=', en: '=', keyCode: '187' },
+  { label: 'Backspace', en: 'Backspace', keyCode: '8' },
+  { label: 'Tab', en: 'Tab', keyCode: '9' },
+  { label: 'й', en: 'q', keyCode: '81' },
+  { label: 'ц', en: 'w', keyCode: '87' },
+  { label: 'у', en: 'e', keyCode: '69' },
+  { label: 'к', en: 'r', keyCode: '82' },
+  { label: 'е', en: 't', keyCode: '84' },
+  { label: 'н', en: 'y', keyCode: '89' },
+  { label: 'г', en: 'u', keyCode: '85' },
+  { label: 'ш', en: 'i', keyCode: '73' },
+  { label: 'щ', en: 'o', keyCode: '79' },
+  { label: 'з', en: 'p', keyCode: '80' },
+  { label: 'х', en: '[', keyCode: '219' },
+  { label: 'ъ', en: ']', keyCode: '221' },
+  { label: '\\', en: '\\', keyCode: '220' },
+  { label: 'del', en: 'del', keyCode: '46' },
+  { label: 'CapsLock', en: 'CapsLock', keyCode: '20' },
+  { label: 'ф', en: 'a', keyCode: '65' },
+  { label: 'ы', en: 's', keyCode: '83' },
+  { label: 'в', en: 'd', keyCode: '68' },
+  { label: 'а', en: 'f', keyCode: '70' },
+  { label: 'п', en: 'g', keyCode: '71' },
+  { label: 'р', en: 'h', keyCode: '72' },
+  { label: 'о', en: 'j', keyCode: '74' },
+  { label: 'л', en: 'k', keyCode: '75' },
+  { label: 'д', en: 'l', keyCode: '76' },
+  { label: 'ж', en: ';', keyCode: '186' },
+  { label: 'э', en: '\\', keyCode: '222' },
+  { label: 'Enter', en: 'Enter', keyCode: '13' },
+  { label: 'Shift', en: 'Shift', keyCode: '16' },
+  { label: 'я', en: 'z', keyCode: '90' },
+  { label: 'ч', en: 'x', keyCode: '88' },
+  { label: 'с', en: 'c', keyCode: '67' },
+  { label: 'м', en: 'v', keyCode: '86' },
+  { label: 'и', en: 'b', keyCode: '66' },
+  { label: 'т', en: 'n', keyCode: '78' },
+  { label: 'ь', en: 'm', keyCode: '77' },
+  { label: 'б', en: ',', keyCode: '188' },
+  { label: 'ю', en: '.', keyCode: '190' },
+  { label: '/', en: '/', keyCode: '191' },
+  { label: '↑', en: '↑', keyCode: '38' },
+  { label: 'Alt', en: 'Alt', keyCode: '18' },
+  { label: 'Ctrl', en: 'Ctrl', keyCode: '17' },
+  { label: 'Alt', en: 'Alt', keyCode: '18' },
+  { label: 'Space', en: 'Space', keyCode: '32' },
+  { label: '←', en: '←', keyCode: '37' },
+  { label: '↓', en: '↓', keyCode: '40' },
+  { label: '→', en: '→', keyCode: '39' },
+  { label: 'Ctrl', en: 'Ctrl', keyCode: '17' },
+];
+
+const screen = document.createElement('textarea');
+screen.focus()
+
+document.body.appendChild(screen);
+
+const keyboard = createKeyboard(keys);
 document.body.appendChild(keyboard);
 
-const screen = document.createElement('div');
+keyboard.classList.add('keyboard');
 screen.classList.add('screen');
-document.body.insertBefore(screen, keyboard);
 
-// Массивы с символами клавиатуры
-const keysEnglishLayout = [
-  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-  ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
-  ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'],
-  ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'],
-  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl'],
-  ['↑', '←', '↓', '→'] // Стрелки
-];
-
-const keysRussianLayout = [
-  ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-  ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
-  ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-  ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.'],
-  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl'],
-  ['↑', '←', '↓', '→'] // Стрелки
-];
+setInterval(function() {
+  document.querySelector('.screen').focus();
+}, 0);
 
 
-// Флаг для определения текущего языка клавиатуры
-let isRussianLayout = false;
+let isCtrlPressed = false;
+let isAltPressed = false;
+let isRussianLayout = true;
+let isCapsLockOn = false;
+let isShiftPressed = false;
 
-// Флаги для отслеживания состояния клавиш Shift и Alt
-let shiftPressed = false;
-let altPressed = false;
+screen.addEventListener('keypress', (event) => {
+  event.preventDefault(); // Запрещаем ввод символа с физической клавиатуры
+});
 
-// Функция для генерации HTML-кода клавиатуры
-function keyboardHTML(layout) {
-  return layout
-    .map(row => {
-      const keysHTML = row.map(key => `<div class="key">${key}</div>`).join('');
-      return `<div class="row">${keysHTML}</div>`;
-    })
-    .join('');
+// Обработчик нажатия клавиши на физической клавиатуре
+document.addEventListener('keydown', (event) => {
+  if(event.key.includes('ArrowLeft') || event.key.includes('ArrowRight') || event.key.includes('ArrowUp') ||  event.key.includes('ArrowDown')) return
+  // Проверяем, была ли уже нажата клавиша на виртуальной клавиатуре
+  const virtualKey = document.querySelector(`button[data-key-code="${event.keyCode}"]`);
+  if (virtualKey && !virtualKey.classList.contains('active')) {
+    virtualKey.classList.add('active');
+    handleKeyClick(virtualKey.textContent);
+  }
+
+  // Проверяем нажатие клавиш Shift, Ctrl и Alt
+  if (event.key === 'Shift') {
+    isShiftPressed = true;
+    updateKeyboardLayout(isRussianLayout, isCapsLockOn, isShiftPressed);
+  }
+  if (event.key === 'Control') {
+    isCtrlPressed = true;
+  }
+  if (event.key === 'Alt') {
+    isAltPressed = true;
+  }
+  
+  // Проверяем состояние Ctrl + Alt для смены языка
+  if (isCtrlPressed && isAltPressed) {
+    isRussianLayout = !isRussianLayout;
+    updateKeyboardLayout(isRussianLayout, isCapsLockOn, isShiftPressed);
+  }
+});
+
+
+// Обработчик отпускания клавиши на физической клавиатуре
+document.addEventListener('keyup', (event) => {
+  // Удаляем активное состояние с нажатой клавиши на виртуальной клавиатуре
+  const virtualKey = document.querySelector(`button[data-key-code="${event.keyCode}"]`);
+  if (virtualKey) {
+    virtualKey.classList.remove('active');
+  }
+
+  // Проверяем отпускание клавиш Shift, Ctrl и Alt
+  if (event.key === 'Shift') {
+    isShiftPressed = false;
+    updateKeyboardLayout(isRussianLayout, isCapsLockOn, isShiftPressed);
+  }
+  if (event.key === 'Control') {
+    isCtrlPressed = false;
+  }
+  if (event.key === 'Alt') {
+    isAltPressed = false;
+  }
+});
+
+
+// Функция для обновления раскладки клавиатуры
+function updateKeyboardLayout(isRussian, isCapsLockOn, isShiftPressed) {
+  keyboard.innerHTML = ''; // Очищаем клавиатуру
+
+  keys.forEach(keyData => {
+    const label = isRussian ? keyData.label : keyData.en;
+    const button = createKeyButton(label, keyData.keyCode, isCapsLockOn, isShiftPressed);
+    keyboard.appendChild(button);
+  });
 }
 
-// Функция для обновления клавиатурного HTML
-function updateKeyboardLayout(layout) {
-  keyboard.innerHTML = keyboardHTML(layout);
-  keyboardKeys = Array.from(keyboard.querySelectorAll('.key')); // Обновляем список клавиш
 
-  // Добавляем обработчики событий для каждой клавиши
-  keyboardKeys.forEach(key => {
-    key.addEventListener('click', () => {
-      const keyText = key.textContent;
-      handleKeyClick(keyText);
-    });
+// Изменение функции handleKeyClick
+// ...
+
+// Измененная функция handleKeyClick
+function handleKeyClick(keyText) {
+  if (keyText === 'Backspace') {
+    screen.value = screen.value.slice(0, -1);
+  } else if (keyText === 'Enter') {
+    screen.value += '\n';
+  } else if (keyText === 'Tab') {
+    screen.value += '\t';
+  } else if (keyText === 'del') {
+    screen.value = '';
+  } else if (keyText === 'CapsLock') {
+    isCapsLockOn = !isCapsLockOn;
+    updateKeyboardLayout(isRussianLayout, isCapsLockOn);
+  } else if (keyText === 'Shift') {
+    // Ничего не делаем при нажатии клавиши Shift
+    return;
+  } else {
+    let isUpperCase = isCapsLockOn && !isRussianLayout;
+    if (isCapsLockOn && isRussianLayout && keyText.match(/[а-яё]/i)) {
+      isUpperCase = !isUpperCase;
+    }
+    if (!isCapsLockOn && !isRussianLayout && keyText.match(/[a-z]/i)) {
+      isUpperCase = !isUpperCase;
+    }
+    screen.value += isUpperCase ? keyText.toUpperCase() : keyText;
+  }
+}
+
+
+
+// ...
+
+
+// Обновляем раскладку клавиатуры на старте
+updateKeyboardLayout(isRussianLayout);
+// Изменение функции createKeyButton
+
+function createKeyButton(label, keyCode, _isCapsLockOn, isShiftPressed) {
+  const button = document.createElement('button');
+  button.textContent = isShiftPressed ? label.toUpperCase() : label;
+  button.dataset.keyCode = keyCode;
+  button.addEventListener('click', () => handleKeyClick(button.textContent));
+  return button;
+}
+
+
+
+function createKeyboard(keys) {
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  keys.forEach(keyData => {
+    const button = createKeyButton(keyData);
+    keyboard.appendChild(button);
   });
 
-  // Обработчик для нажатия клавиш на физической клавиатуре
-window.addEventListener('keydown', handleKeyDown);
-window.addEventListener('keyup', handleKeyUp);
+  return keyboard;
 }
 
-// Функция для обработки клика по клавише
-function handleKeyClick(keyText) {
-if (keyText === 'Backspace') {
-screen.textContent = screen.textContent.slice(0, -1);
-} else if (keyText === 'Enter') {
-screen.textContent += '\n';
-} else if (keyText === 'Tab') {
-screen.textContent += '\t';
-} else {
-screen.textContent += keyText;
-}
-}
+// Добавляем обработчики событий для каждой клавиши
+// получаем все элементы клавиатуры
+const keyboardKeys = document.querySelectorAll('.key');
+
+// добавляем обработчик событий на каждую клавишу
+keyboardKeys.forEach(key => {
+  key.addEventListener('click', () => {
+    screen.value += key.textContent; // добавляем символ в текстовое поле
+  });
+});
 
 
-// Функция для переключения раскладки клавиатуры
-function toggleKeyboardLayout() {
-isRussianLayout = !isRussianLayout;
-if (isRussianLayout) {
-updateKeyboardLayout(keysRussianLayout);
-} else {
-updateKeyboardLayout(keysEnglishLayout);
-}
-}
 
-// Функция для обработки нажатия клавиш на физической клавиатуре
-function handleKeyDown(event) {
-const { key, code } = event;
-if (key === 'Shift' || code === 'ShiftLeft' || code === 'ShiftRight') {
-shiftPressed = true;
-} else if (key === 'Alt' || code === 'AltLeft' || code === 'AltRight') {
-altPressed = true;
-} else if (key === 'Control' || code === 'ControlLeft' || code === 'ControlRight') {
-// Добавьте обработку нажатия клавиши Control, если необходимо
-} else {
-const keyText = mapPhysicalKeyToVirtualKey(key);
-handleKeyClick(keyText);
-}
-}
 
-// Функция для обработки отпускания клавиш на физической клавиатуре
-function handleKeyUp(event) {
-const { key, code } = event;
-if (key === 'Shift' || code === 'ShiftLeft' || code === 'ShiftRight') {
-shiftPressed = false;
-} else if (key === 'Alt' || code === 'AltLeft' || code === 'AltRight') {
-altPressed = false;
-} else if (key === 'Control' || code === 'ControlLeft' || code === 'ControlRight') {
-// Добавьте обработку отпускания клавиши Control, если необходимо
-}
-}
-
-// Функция для отображения символа виртуальной клавиатуры
-function mapPhysicalKeyToVirtualKey(key) {
-// Добавьте соответствующие маппинги для физических клавиш
-// в соответствии с выбранной раскладкой клавиатуры и состоянием клавиш Shift и Alt
-// Например:
-if (isRussianLayout) {
-if (key === 'a' && !shiftPressed) {
-return 'ф';
-} else if (key === 'a' && shiftPressed) {
-return 'А';
-}
-// Добавьте другие маппинги для русской раскладки
-} else {
-if (key === 'ф' && !shiftPressed) {
-return 'a';
-} else if (key === 'ф' && shiftPressed) {
-return 'A';
-}
-
-// Добавьте другие маппинги для английской раскладки
-}
-
-// Если необходимо добавить маппинги для других клавиш,
-// можно использовать условные операторы или switch-конструкцию
-
-// Если не найден соответствующий символ, вернуть исходный ключ
-return key;
-}
-
-// Инициализация клавиатуры
-updateKeyboardLayout(keysEnglishLayout);
-
-// Функция для смены раскладки клавиатуры при нажатии клавиши Shift
-function handleShiftKey() {
-shiftPressed = !shiftPressed;
-if (shiftPressed) {
-toggleKeyboardLayout(); // Переключаемся на другую раскладку
-} else {
-if (isRussianLayout) {
-toggleKeyboardLayout(); // Переключаемся обратно на русскую раскладку
-}
-}
-}
-
-// Обработчик события для нажатия клавиши Shift
-keyboardKeys.find(key => key.textContent === 'Shift').addEventListener('click', handleShiftKey);
-
-// Обработчик события для нажатия клавиши Caps Lock
-keyboardKeys.find(key => key.textContent === 'Caps Lock').addEventListener('click', toggleKeyboardLayout);
-
-// При запуске скрипта используем английскую раскладку клавиатуры
-updateKeyboardLayout(keysEnglishLayout);
 
